3d32ea3d56353e25b11447f6b68ea1485f5f5b45
karloseasilva-netizen <karloseasilva@gmail.com>
2026-07-28 03:19:28 -0300

    Add migration to create product-images storage bucket

---
+++ supabase/migrations/20260728032000_create_product_images_bucket.sql
@@ -0,0 +1,4 @@
+-- Create the product-images bucket if it doesn't exist
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('product-images', 'product-images', true)
+ON CONFLICT (id) DO NOTHING;
